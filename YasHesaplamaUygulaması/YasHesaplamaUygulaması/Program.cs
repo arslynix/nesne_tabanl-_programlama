2c38163475b29a04fe7f5759f33bb140d05b699f
arslynix <busearslan085@gmail.com>
2024-12-17 16:12:43 +0300

    Add files via upload

---
+++ YasHesaplamaUygulaması/YasHesaplamaUygulaması/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YasHesaplamaUygulaması
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Doğum tarihinizi girin. (GG//AA/YYYY) formatında");
+            string input = Console.ReadLine();
+
+            if (DateTime.TryParse(input, out DateTime birthDate))
+            {
+                DateTime today = DateTime.Today;
+                int age = today.Year - birthDate.Year;
+                Console.WriteLine("Yaşınız: " + age);
+            }
+            else
+            {
+                Console.WriteLine("Geçersiz tarih formatı.");
+            }
+        }
+    }
+}
