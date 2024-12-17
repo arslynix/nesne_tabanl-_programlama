b411eebd19923260ff6c0181ca751c8bdf7e22f7
arslynix <busearslan085@gmail.com>
2024-12-17 20:45:47 +0300

    Add files via upload
    This project demonstrates the concept of inheritance and the use of the base keyword in C#. The base keyword is used to call the constructor or methods of a base class from a derived class. In the example, the Car class inherits from the Vehicle class, and the base constructer is called to initialize properties of the base class.

---
+++ base/base/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace @base
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car("Tesla", 120, 4);
+            car.DisplayInfo();
+        }
+    }
+    public class Vehicle
+    {
+        public string Brand { get; set; }
+        public int MaxSpeed { get; set; }
+        public Vehicle(string brand, int maxSpeed )
+        {
+            Brand = brand;
+            MaxSpeed = maxSpeed;
+        }
+    }
+    public class Car : Vehicle
+    {
+        public int NumberOfDoors { get; set; }
+        public Car(string brand, int maxSpeed, int NumberOfDoors) :base(brand, maxSpeed)
+        {
+            this.NumberOfDoors = NumberOfDoors;
+        }
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Brand: {Brand}, Max Speed: {MaxSpeed}km/h, NumberOfDoors: {NumberOfDoors}");
+        }
+    }
+
+}
