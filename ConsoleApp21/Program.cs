73c1ad95ddea6556c0024441f29ccc4bb589b39c
arslynix <busearslan085@gmail.com>
2024-12-18 15:00:17 +0300

    Add files via upload

---
+++ ConsoleApp21/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp21
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+		    try
+			{
+				int yas = -5;
+				if (yas < 0)
+				{
+					throw new Exception("Yaş negatif olamaz.");
+				}
+			}
+			catch (CustomException ex)
+			{
+				Console.WriteLine("Özel Hata:" + ex.Message);
+			}
+		}
+		public class CustomException : Exception
+		{
+			public CustomException(string message) : base(message) { }
+		}
+		
+	}
+}
