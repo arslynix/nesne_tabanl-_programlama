fa3800359b8c74ab7b2e02e844979ae9db67e856
arslynix <busearslan085@gmail.com>
2024-12-18 14:37:02 +0300

    Add files via upload

---
+++ ConsoleApp20/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp20
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			try
+			{
+				Console.Write("Bir sayı girin");
+				int number = int.Parse(Console.ReadLine());
+				Console.WriteLine("Girilen sayı: " + number);
+			}
+			catch (FormatException ex)
+			{
+				Console.WriteLine(" Hata: Geçersiz bir sayı girdiniz.");
+			}
+			finally
+			{
+				Console.WriteLine(" Girdi işlemi tamamlandı.");
+					
+			}
+			Console.ReadLine();
+		}
+	}
+}
