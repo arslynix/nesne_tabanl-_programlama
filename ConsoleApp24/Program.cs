ba8b7bb656c01825acc1f6aa11ec0718c4e88768
arslynix <busearslan085@gmail.com>
2024-12-19 14:43:24 +0300

    Add files via upload

---
+++ ConsoleApp24/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp24
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int stok = 5;
+			Console.WriteLine("stok miktarı: " + stok);
+			try
+			{
+				Console.Write("Almak istediğiniz ürün miktarını girin: ");
+				int miktar = int.Parse(Console.ReadLine());
+				if (miktar > stok)
+				{
+					throw new OutOfMemoryException("Yetersiz stok.");
+				}
+				stok -= miktar;
+				Console.WriteLine($"Satın alma başarılı. Kalan stok: {stok}");
+			}
+			catch (FormatException)
+			{
+				Console.WriteLine("Lütfen geçerli bir sayı girin.");
+			}
+			catch (OutOfStockException ex)
+			{
+				Console.WriteLine(ex.Message);
+			}
+		}
+
+	}
+	public class OutOfStockException: Exception
+	{
+		public OutOfStockException(string message) : base(message) { }
+	}
+}
