2b656cfee4417cd242f5200a64c56c8f7eeeeb7c
arslynix <busearslan085@gmail.com>
2024-12-30 20:20:17 +0300

    Add files via upload

---
+++ Polymorphism_ParentChild/Polymorphism_ParentChild/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism_ParentChild
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Parent parent = new Parent();
+            Parent ChildAsParent = new Child();
+            Child child = new Child();
+
+            parent.Show();
+            ChildAsParent.Show();
+            child.Show();
+            Console.ReadLine();
+        }
+    }
+    public class Parent
+    {
+        public virtual void Show()
+        {
+            Console.WriteLine("Parent's show method");
+        }
+    }
+    public class Child: Parent
+    {
+        public new void Show()
+        {
+            Console.WriteLine("Child's show method");
+        }
+    }
+}
