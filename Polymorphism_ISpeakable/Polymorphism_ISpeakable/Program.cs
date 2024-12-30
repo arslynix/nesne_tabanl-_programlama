be7bbbe87fa959eccf99245564465efe4d0efc91
arslynix <busearslan085@gmail.com>
2024-12-30 20:20:49 +0300

    Add files via upload

---
+++ Polymorphism_ISpeakable/Polymorphism_ISpeakable/Program.cs
@@ -0,0 +1,41 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism_ISpeakable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ISpeakable[] animals = { new Dog(), new Cat() };
+
+            foreach (ISpeakable animal in animals)
+            {
+                animal.Speak();                
+            }
+            Console.ReadLine();
+        }
+    }
+    public interface ISpeakable
+    {
+        void Speak();
+    }
+    public class Cat: ISpeakable
+    {
+        public void Speak()
+        {
+            Console.WriteLine("Cat meows.");
+        }
+    }
+    public class Dog: ISpeakable
+    {
+        public void Speak()
+        {
+            Console.WriteLine("Dog barks.");
+        }
+    }
+}
